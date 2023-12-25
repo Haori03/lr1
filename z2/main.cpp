a752ebb503976599e68480dc9adc9f9bbc0806e3
Haori03 <145291197+Haori03@users.noreply.github.com>
2023-12-26 00:29:43 +0300

    main.cpp

--- z2/main.cpp
+++ z2/main.cpp
@@ -1,44 +1,21 @@
-#include "modAlphaCipher.h"
+#include "cipher.h"
 
-bool isValid(const string& s)
-{
-    for(auto c:s)
-        if (!isalpha(c)||!isupper(c))
-            return false;
-    return true;
-}
-int main(int argc, char **argv)
-{
+int main() {
     int key;
-    string st;
-    unsigned op;
-    cout<<"ВВЕДИТЕ КЛЮЧ (ЧИСЛО) : ";
-    cin>>key;
-    if (!cin.good()) {
-        clog<<"КЛЮЧ НЕ ПОДХОДИТ\n";
-        return 1;
+    std::cout << "Введите ключ: ";
+    std::cin >> key;
+    Cipher cipher(key);
+    std::string text;
+    std::cout << "Введите текст: ";
+    std::cin >> text;
+    int sw;
+    std::cout << "Для шифрования введите - 1 \nДля расшифровки введите - 2 -->";
+    std::cin >> sw;
+    if (sw == 1){
+        std::cout << "Зашифрованный текст: " << cipher.encrypt(text) << std::endl;
     }
-    cout<<"КЛЮЧ ПРИНЯТ\n";
-    modAlphaCipher cipher(key);
-    do {
-        cout<<"ВЫБЕРИТЕ ОПЕРАЦИЮ (0-ВЫХОД, 1-ЗАШИФРОВАТЬ, 2-РАСШИФРОВАТЬ): ";
-        cin>>op;
-        if (op > 2) {
-            cout<<"ОШИБКА В ВЫБОРЕ ОПЕРАЦИИ\n";
-        } else if (op >0) {
-            cout<<"ВВЕДИТЕ СТРОКУ, СОСТОЯЩУЮ ИЗ ЛАТИНИЦЫ : ";
-            cin>>st;
-            if (isValid(st)) {
-                if (op==1) {
-                    cout<<"ЗАШИФРОВАННАЯ СТРОКА: " << cipher.encrypt(st) << endl;
-                } else {
-                    cout<<"РАСШИФРОВАННАЯ СТРОКА: " << cipher.decrypt(st) << endl;
-                }
-            } else {
-                cout<<"НЕКОРРЕКТНАЯ СТРОКА.\n";
-            }
-        }
-    } while (op!=0);
-
+    if (sw == 2){
+        std::cout << "Расшифрованный текст: " << cipher.decrypt(text) << std::endl;
+    }
     return 0;
 }

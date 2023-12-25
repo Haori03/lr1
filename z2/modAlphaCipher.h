f397418a31181f7bae6af8df1974f236f0651393
Haori03 <145291197+Haori03@users.noreply.github.com>
2023-12-26 00:30:28 +0300

    modAlphaCipher.h

--- z2/modAlphaCipher.h
+++ z2/modAlphaCipher.h
@@ -1,21 +1,13 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <cctype>
-#include <codecvt>
-#include <vector>
-#include <map>
 #include <algorithm>
-#include <locale>
-using namespace std;
 
-class modAlphaCipher
-{
-private:
-    int newkey; // количество столбцов таблицы
+class Cipher {
 public:
-    modAlphaCipher()=delete;
-    modAlphaCipher(const int& key) :newkey(key) {}; // конструктор
-    string encrypt(const string& open_st); // зашифрование
-    string decrypt(const string& cipher_st); // расшифрование
+    Cipher(int key) : key_(key) {}
+    std::string encrypt(std::string text);
+    std::string decrypt(std::string text);
+private:
+    int key_;
 };
